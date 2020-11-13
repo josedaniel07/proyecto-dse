ab9069629b1329dc5c565f3c06b0b51ebabdb968
Jose Daniel <bacajosedaniel@gmail.com>
2020-11-13 15:31:11 -0500

    lab 5

---
+++ main/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('productos',views.ProductListView.as_view(), name='product-list'),
+    path('productos/<int:pk>',views.ProductDetailView.as_view(), name='product-detail'),
+    path('proveedores', views.ProveedorListView.as_view(), name='proveedor-list'),
+    path('proveedor/<str:pk>', views.ProveedorDetailView.as_view(), name='proveedor-detail'),
+    path('categorias', views.CategoriaListView.as_view(), name='categoria-list'),
+    path('categorias/<str:pk>', views.CategoriaDetailView.as_view(), name='categoria-detail'),
+    path('localizacion', views.LocalizacionListView.as_view(), name='localizacion-list'),
+    path('localizacion/<str:pk>', views.LocalizacionDetailView.as_view(), name='localizacion-detail'),
+    path('registro/', views.RegistrationView.as_view(), name='register'),
+    path('add_to_cart/<int:product_pk>',views.AddToCartView.as_view(), name='add-to-cart'),
+    path('remove_from_cart/<int:product_pk>', views.RemoveFromCartView.as_view(), name='remove-from-cart'),
+    path('carrito/', views.PedidoDetailView.as_view(), name='pedido-detail'),
+    path('checkout/<int:pk>', views.PedidoUpdateView.as_view(), name='pedido-update'),
+    path('payment/', views.PaymentView.as_view(), name='payment'),
+    path('complete_payment/', views.CompletePaymentView.as_view(), name='complete-payment'),
+]
